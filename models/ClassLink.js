de7db53ffbb42421ec48babc82fc2472d489ae04
Christofearans <tebogorector2@gmail.com>
2024-12-16 16:55:08 +0200

    Fix ClassLink model overwrite issue

--- models/ClassLink.js
+++ models/ClassLink.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
 
 const classLinkSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+  title: String,
+  link: String,
+  timestamp: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.models.ClassLink || mongoose.model('ClassLink', classLinkSchema);
+module.exports = mongoose.model('ClassLink', classLinkSchema);
