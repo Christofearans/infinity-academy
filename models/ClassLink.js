05f1aeb31cd16d8d8801ea1221e60a01cd860f06
Christofearans <tebogorector2@gmail.com>
2024-12-16 16:20:17 +0200

    Fix classLinksRoutes to handle class links

---
+++ models/ClassLink.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const classLinkSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('ClassLink', classLinkSchema);
+ 
