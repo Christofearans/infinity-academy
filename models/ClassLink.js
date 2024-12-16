26fbee2d1ef94657f6f70002d4740c6753ca5dd6
Christofearans <tebogorector2@gmail.com>
2024-12-16 17:03:09 +0200

    Resolve merge conflict in ClassLink.js

--- models/ClassLink.js
+++ models/ClassLink.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
 const classLinkSchema = new mongoose.Schema({
-  title: String,
-  link: String,
-  timestamp: { type: Date, default: Date.now },
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-<<<<<<< HEAD
-module.exports = mongoose.model('ClassLink', classLinkSchema);
-=======
 module.exports = mongoose.models.ClassLink || mongoose.model('ClassLink', classLinkSchema);
-
->>>>>>> 7827409024688337fea1c7e55b89a5aa16bb3282
