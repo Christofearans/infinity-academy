dcd696a9b1f78cad0b7b71e003cc92552571b93f
Christofearans <tebogorector2@gmail.com>
2024-12-18 15:24:44 +0200

    Announcement.js

--- models/Announcement.js
+++ models/Announcement.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const announcementSchema = new mongoose.Schema({
-  text: String,
-  date: String,
+  content: { type: String, required: true },  // Ensure content is required
+  image: { type: String },  // Optional field for image URL
+  timestamp: { type: Date, default: Date.now }  // Add timestamp with default value
 });
 
 module.exports = mongoose.model('Announcement', announcementSchema);
+
