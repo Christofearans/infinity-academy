b81b6e8a787bd5615a2ef95b5981e1f30e5304a8
Christofearans <tebogorector2@gmail.com>
2024-12-18 15:00:54 +0200

    User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: String, // 'admin', 'tutor', 'student'
-  schoolType: String, // 'highschool', 'university'
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true }, // 'admin', 'tutor', 'student'
+  schoolType: { type: String }, // 'highschool', 'university'
+  lastSeen: { type: Date }, // Add this field
+  totalTimeSpent: { type: Number, default: 0 } // Add this field to track time spent
 });
 
 module.exports = mongoose.model('User', userSchema);
+
