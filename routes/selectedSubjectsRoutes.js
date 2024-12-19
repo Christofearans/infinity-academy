d139602e6fb9e7463a01db2a811992ea5f8b2bb4
Christofearans <tebogorector2@gmail.com>
2024-12-19 08:25:10 +0200

    selectedSubjectsRoutes.js

--- routes/selectedSubjectsRoutes.js
+++ routes/selectedSubjectsRoutes.js
@@ -4,16 +4,25 @@ const SelectedSubjects = require('../models/SelectedSubjects');
 
 // Post selected subjects
 router.post('/selectedSubjects', async (req, res) => {
-  const { selectedSubjects } = req.body;
-  const newSelectedSubjects = new SelectedSubjects({ selectedSubjects });
-  await newSelectedSubjects.save();
-  res.status(201).send('Selected subjects saved');
+  try {
+    const { selectedSubjects } = req.body;
+    const newSelectedSubjects = new SelectedSubjects({ selectedSubjects });
+    await newSelectedSubjects.save();
+    res.status(201).send('Selected subjects saved');
+  } catch (error) {
+    res.status(500).json({ message: 'Error saving selected subjects', error });
+  }
 });
 
 // Get selected subjects
 router.get('/selectedSubjects', async (req, res) => {
-  const selectedSubjects = await SelectedSubjects.find();
-  res.json(selectedSubjects);
+  try {
+    const selectedSubjects = await SelectedSubjects.find();
+    res.json(selectedSubjects);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching selected subjects', error });
+  }
 });
 
 module.exports = router;
+
