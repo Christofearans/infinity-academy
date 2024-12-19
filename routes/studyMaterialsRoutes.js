52270e62b428b9d28ed30f935725f217aae0c844
Christofearans <tebogorector2@gmail.com>
2024-12-19 20:47:47 +0200

    studyMaterialsRoutes.js

--- routes/studyMaterialsRoutes.js
+++ routes/studyMaterialsRoutes.js
@@ -5,21 +5,33 @@ const StudyMaterial = require('../models/StudyMaterial');
 // Post a new study material
 router.post('/studyMaterials', async (req, res) => {
   const { subject, type, title, link, timestamp } = req.body;
-  const newStudyMaterial = new StudyMaterial({ subject, type, title, link, timestamp });
-  await newStudyMaterial.save();
-  res.status(201).send('Study material uploaded');
+  try {
+    const newStudyMaterial = new StudyMaterial({ subject, type, title, link, timestamp });
+    await newStudyMaterial.save();
+    res.status(201).send('Study material uploaded');
+  } catch (error) {
+    res.status(500).json({ message: 'Error uploading study material', error });
+  }
 });
 
 // Get all study materials
 router.get('/studyMaterials', async (req, res) => {
-  const studyMaterials = await StudyMaterial.find();
-  res.json(studyMaterials);
+  try {
+    const studyMaterials = await StudyMaterial.find();
+    res.json(studyMaterials);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching study materials', error });
+  }
 });
 
 // Delete a study material
 router.delete('/studyMaterials/:id', async (req, res) => {
-  await StudyMaterial.findByIdAndDelete(req.params.id);
-  res.send('Study material deleted');
+  try {
+    await StudyMaterial.findByIdAndDelete(req.params.id);
+    res.send('Study material deleted');
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting study material', error });
+  }
 });
 
 module.exports = router;
